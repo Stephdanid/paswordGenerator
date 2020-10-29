b0430fe8f3c88b5b7b5e9241f375c7f84ec884fb
stephdanid <stephaniedanidominguez@gmail.com>
2020-10-28 20:22:25 -0500

    Arrays created, initial question function is started but not working. but other confirm options follow in that function

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+// Arrays splitting to make them individual strings
+let lowerCase = "abcdefghijklmnopqrstuvxyz".split("");
+let upperCase = "ABCDEGHIJKLMNOPQRSTUVXYZ".split("");
+let specialChar = "!@#$%^&*()".split("");
+let numberChar = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+//Initial question and popping alert for userInput WHY DONT WORK
+function passwordQuest() {
+  let userInput = prompt("How many characters would you like to use?")
+  if (userInput < 8 || userInput > 128 || isNaN(userInput)) {
+    alert("This page needs a number to continue")
+    return
+  }
+  // just placing other options for now.
+let lCaseOpt = confirm("Would you like to have lower case letters in your password?");
+let uCaseOpt = confirm("Would you like to have upper case letters in your password?");
+let spcCharOpt = confirm("Would you like to have special characters in your password?");
+let numCharOpt = confirm("Would you like to have numbers in your password?")
+}
+
+
+// 
+
+
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
